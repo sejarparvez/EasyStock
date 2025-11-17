dcd0cfbc13f79e3c996a0b7b5abb03e4748a8d0f
Sejar Parvez <sejarparvez@yahoo.com>
2025-11-17 13:38:09 +0600

    added prisma

---
+++ lib/prisma.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from './generated/prisma/client';
+
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error'],
+  });
+};
+
+// biome-ignore lint/suspicious/noShadowRestrictedNames: error
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const db = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default db;
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prismaGlobal = db;
+}
