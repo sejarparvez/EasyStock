a09f08d27fcc1bb983875ae584e91b6b4d77176c
Sejar Parvez <sejarparvez@yahoo.com>
2025-11-26 18:46:53 +0600

    improved prisma schema

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,9 @@
-import { PrismaClient } from '@/lib/generated/prisma';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { PrismaClient } from '@/lib/generated/prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
